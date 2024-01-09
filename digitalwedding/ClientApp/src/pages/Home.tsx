a07cf1415b762e2662e114eea2a3d6ec4750ce13
Guillermo Melean <guial@live.com.pt>
2024-01-09 03:50:10 +0000

    Add css to dialog content

--- digitalwedding/ClientApp/src/pages/Home.tsx
+++ digitalwedding/ClientApp/src/pages/Home.tsx
@@ -67,7 +67,7 @@ export function Home() {
 
               <Dialog>
                 <DialogTrigger className='inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[#6F8374] text-primary-foreground hover:bg-[#6F8374]/90 h-10 px-4 py-2 mt-8'>Subscreva a newsletter</DialogTrigger>
-                <DialogContent>
+                <DialogContent className="rounded-lg w-[95%]">
                   <DialogHeader className="mt-3">
                     <DialogTitle>Não perca o nosso lançamento!</DialogTitle>
                     <DialogDescription>
