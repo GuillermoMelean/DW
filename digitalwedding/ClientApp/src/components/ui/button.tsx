43f8494849ea766ce46501db60033d4a82131c43
Guillermo Melean <guial@live.com.pt>
2024-01-09 03:44:04 +0000

    Improve Home Page

--- digitalwedding/ClientApp/src/components/ui/button.tsx
+++ digitalwedding/ClientApp/src/components/ui/button.tsx
@@ -18,6 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        first: "bg-[#38463B] text-primary-foreground hover:bg-primary/90",
+        second: "bg-[#6F8374] text-primary-foreground hover:bg-[#6F8374]/90"
       },
       size: {
         default: "h-10 px-4 py-2",
--- digitalwedding/ClientApp/src/pages/Home.tsx
+++ digitalwedding/ClientApp/src/pages/Home.tsx
@@ -1,18 +1,112 @@
-import logo from '../assets/logo.svg';
+import * as z from "zod"
+// import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form";
+// import { toast } from "react-hot-toast";
+// import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 import '../styles/Home.css';
-import { Button } from '../components/ui/button';
 import bg from '../assets/login-bg.jpg'
+import logo from '../assets/logo.png'
+
+import { Input } from "../components/ui/input";
+import { Button } from '../components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../components/ui/dialog"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "../components/ui/form";
+
+const formSchema = z.object({
+  email: z.string().email(),
+});
 
 export function Home() {
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: ""
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // try {
+    //   setLoading(true);
+    //   const response = await axios.post('/api/weddings', values);
+    //   window.location.assign(`/${response.data.id}`);
+    // } catch (error) {
+    //   toast.error('Something went wrong');
+    // } finally {
+    //   setLoading(false);
+    // }
+  };
+
   return (
     <div className="App " style={{ backgroundImage: `url(${bg})` }}>
-      <header className="flex justify-center items-center w-full">
+      <header className="flex justify-center items-center w-full bg-[#38463b66]" >
         <div className="justify-center items-center row-auto">
           <div className="mt-5 row-auto">
             <div className="bg-white mx-5 border form-wrapper flowers neela-style col-md-12 rounded-lg p-12">
-              <h2 className="section-title baseboard">Digital Wedding</h2>
-              <h5 className="section-title">Brevemente disponível</h5>
+              <h2 className="flex justify-center section-title baseboard mb-8"><img className='w-[400px]' src={logo} /></h2>
               <p className="text-center">Junte-se a nós e comece a construir o site perfeito para tornar o seu grande dia inesquecível!</p>
+
+              <Dialog>
+                <DialogTrigger className='inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-[#6F8374] text-primary-foreground hover:bg-[#6F8374]/90 h-10 px-4 py-2 mt-8'>Subscreva a newsletter</DialogTrigger>
+                <DialogContent>
+                  <DialogHeader className="mt-3">
+                    <DialogTitle>Não perca o nosso lançamento!</DialogTitle>
+                    <DialogDescription>
+                      O seu e-mail está seguro connosco!
+                      Não partilhamos com terceiros, apenas boas notícias diretamente para a sua caixa de entrada."
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div>
+                    <div>
+                      <div className="space-y-4 py-2 pb-4">
+                        <div className="space-y-2">
+                          <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)}>
+                              <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                  <FormItem>
+                                    <FormLabel>E-mail</FormLabel>
+                                    <FormControl>
+                                      <Input disabled={loading} placeholder="Insira o seu endereço de e-mail" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                  </FormItem>
+                                )}
+                              />
+                              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+
+                                <Button disabled={loading} type="submit">Subscrever</Button>
+                              </div>
+                            </form>
+                          </Form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </DialogContent>
+              </Dialog>
+
             </div>
           </div>
         </div>
